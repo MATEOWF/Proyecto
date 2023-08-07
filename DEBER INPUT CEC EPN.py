6430b27462a3bac95e7d131ca458acd02a5a55c7
MATEOWF <141183389+MATEOWF@users.noreply.github.com>
2023-08-06 21:25:49 -0500

    Add files via upload

---
+++ DEBER INPUT CEC EPN.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 15:22:03 2023
+
+@author: Mateo Farinango
+"""
+
+#INPUT
+#SOLCIITAR DATOS DEL USUARIO
+nombre=input("Ingrese su nombre: ")
+apellido=(input("Ingrese su apellido: "))
+ubicacion=(input("Ingrese su ubicacion actual: "))
+edad=int(input("Ingrese su edad: "))
+
+#PARRAFO CON PRINT
+print("\nUSUARIO:",nombre,apellido,",","EDAD:",edad,"años",",","UBICACION:",ubicacion)
+
+
+
+
+
+
+
+
